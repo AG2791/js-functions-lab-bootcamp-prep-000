49d484b0de7a47dc98fd5ebb81e9f83f85ef619b
AJ Guiles <ashiguiles@gmail.com>
2019-01-04 03:18:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ function happyHolidayTo(holiday, name){
   
 }
 
-function holidayCountdown(days, holiday){
-return `It\'s ${days} days until ${holiday}!}
 
+
+function holidayCountdown(holiday, days){
+  return `It\'s ${days} days until ${holiday}!`
+}
